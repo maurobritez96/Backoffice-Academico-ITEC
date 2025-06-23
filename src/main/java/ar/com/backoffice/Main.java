b9fd6869afe55c7debd9c4cce774d029f2bd5140
Mauro Britez <maurobritez96@gmail.com>
2025-06-22 21:12:38 -0300

    feat: mensaje del main

--- src/main/java/ar/com/backoffice/Main.java
+++ src/main/java/ar/com/backoffice/Main.java
@@ -2,6 +2,6 @@ package ar.com.backoffice;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello World");
     }
 }
